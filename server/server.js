d317b532f52490ca5b59abb93df51aa8eb1a92b1
Y-Linh <sailinh1010@gmail.com>
2025-04-22 03:30:22 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '20mb' }));
+// Cấu hình Express để phục vụ các tệp tĩnh từ thư mục build của React
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('MongoDB connected'))
@@ -129,6 +131,10 @@ app.get('/api/max-id/:category', verifyToken, async (req, res) => {
         res.status(500).json({ message: 'Lỗi khi tìm ID cao nhất' });
     }
 });
+// Nếu không tìm thấy route nào (ví dụ khi người dùng truy cập vào /), gửi index.html của React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
-
+const PORT = process.env.PORT || 5000;
 app.listen(5000, () => console.log('🚀 Server is running at http://localhost:5000'));
